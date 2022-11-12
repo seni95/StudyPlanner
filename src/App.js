b75c44a82f30d381037a61a1beb13bcc3d2753e8
Seyeon <ghkdtpdus95@naver.com>
2022-11-12 15:33:31 +0900

    add calendar function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import DayPlanner from './components/DayPlanner/DayPlanner';
+import Calendar from './components/Calendar/Calendar';
 
 function App({plannerRepository}) {
   return (
   <div>
     <DayPlanner plannerRepository={plannerRepository}></DayPlanner>
+    <Calendar></Calendar>
   </div>
   );
 }
